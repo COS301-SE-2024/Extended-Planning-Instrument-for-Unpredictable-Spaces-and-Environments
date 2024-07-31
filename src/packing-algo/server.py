d53cd2f645246fb3709577d2008db9826e982da3
Mattvc-03 <u22491199@tuks.co.za>
2024-07-31 14:59:19 +0200

    Edits to truck

--- src/packing-algo/server.py
+++ src/packing-algo/server.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, emit, join_room
+import os
 import math
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Store the container and boxes data in memory
+# Mock initialization of containers and boxes data
 data = {
-    "containers": [],  # Now handles multiple containers
+    "containers": [
+        {
+            "id": 1,
+            "width": 100,
+            "height": 100,
+            "length": 100,
+            "boxes": []
+        }
+    ],
     "generations": []
 }
 
@@ -15,9 +24,16 @@ data = {
 def index():
     return render_template('index.html')
 
+@app.route('/truck/<path:filename>')
+def truck_files(filename):
+    print("Request for truck file:", filename)  # Debug statement
+    print("Directory contents:", os.listdir(os.path.join('templates', 'truck')))  # Debug statement
+    return send_from_directory(os.path.join('templates', 'truck'), filename)
+
 @socketio.on('connect')
 def handle_connect():
     join_room('main_room')  # Join the default room
+    print("Client connected. Sending initial data:", data)  # Debug statement
     emit('update_data', data, room='main_room')
 
 def fits(container, box, x, y, z):
@@ -84,6 +100,7 @@ def handle_add_box(box_data):
     for container in data['containers']:
         if add_box_to_container(container, box_data):
             break
+    print("Data after adding box:", data)  # Debug statement
     emit('update_data', data, room='main_room', broadcast=True)
 
 @socketio.on('update_generation')
@@ -92,6 +109,7 @@ def handle_update_generation(generation_data):
         container['layers'] = organize_boxes_into_layers(container)
         del container['boxes']  # Remove the flat list of boxes
     data["generations"] = [generation_data]  # Store only the latest generation data
+    print("Data after updating generation:", data)  # Debug statement
     emit('update_data', data, room='main_room', broadcast=True)
 
 @socketio.on('remaining_volume')
