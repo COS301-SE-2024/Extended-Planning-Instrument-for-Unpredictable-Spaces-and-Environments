52f4ec120f532649b1f080c7f35d6e14eec2b0cd
Joshua Joseph <75155367+Joshua-Joseph1@users.noreply.github.com>
2024-08-11 16:43:13 +0200

    chore: Update environment variables for deployment

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseKey = process.env.VITE_SUPABASE_KEY;
 
 console.log("Supabase URL:", supabaseUrl);
 console.log("Supabase Key:", supabaseKey);
