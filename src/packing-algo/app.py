7c0fc3460e8a5e9f5e6bc8cf67c86b3a3d1c90a4
Mr-KSpolidoro <keganspoli@gmail.com>
2024-08-28 17:00:40 +0200

    deployed python algorithm with docker and google cloud

--- src/packing-algo/app.py
+++ src/packing-algo/app.py
@@ -1,24 +1,35 @@
+import os
 from flask import Flask, request, jsonify
-import socketio
 from supabase import create_client, Client
 from packing import genetic_algorithm  
 import json
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
+load_dotenv()
+
 # Initialize Supabase client
-url: str = "https://rgisazefakhdieigrylb.supabase.co/"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnaXNhemVmYWtoZGllaWdyeWxiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNjMxMzE1MSwiZXhwIjoyMDMxODg5MTUxfQ.ctQmfWfRjY77afjwWuynIL4lRdjrtBD7Xqh75SxQBeo"
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+
 supabase: Client = create_client(url, key)
 
 @app.route('/uploadSolution', methods=['POST'])
 def get_solution():
     data = request.json
     shipment_id = data.get('shipmentID')
+    container_dimensions = data.get('containerSize',[])
+    
+    box_data = data.get('boxes', {}).get('data', [])
 
-    # Validate shipment_id
+    # Validate shipment_id and box_data
     if shipment_id is None or not isinstance(shipment_id, int):
         return jsonify({"error": "Invalid shipmentID provided"}), 400
+    if len(container_dimensions) != 3:
+        return jsonify({"error": "Invalid containerSize provided"}), 400
+    if not box_data:
+        return jsonify({"error": "No box data provided"}), 400
 
     class MockSocketIO:
         def emit(self, event, data):
@@ -26,23 +37,21 @@ def get_solution():
 
     sio = MockSocketIO()
 
-    container_dimensions = (1200, 1380, 2800)  # width, height, depth
-
-    # Run the genetic algorithm to get the solution (uncomment when needed)
-    solution_json = genetic_algorithm('products.csv', container_dimensions, sio)
-    # For now, using a test solution string
+    # container_dimensions = (1200, 1380, 2800) 
+    container_width, container_height, container_depth = container_dimensions
+    # Run the genetic algorithm with the provided box data
+    solution_json = genetic_algorithm(box_data, (container_width, container_height, container_depth), sio)
     solution_string = json.dumps(solution_json)
 
     # Upload the solution to the Supabase bucket (private)
-    file_name = f"{shipment_id}.json"  # Name the file using shipmentId
-    file_path = f"solutions/{file_name}"  # Define the path in the bucket
+    file_name = f"{shipment_id}.json"
+    file_path = f"solutions/{file_name}"
 
     try:
         # Upload the file
         response = supabase.storage.from_("solutions").upload(file_path, solution_string.encode('utf-8'))
         print("File upload response:", response)
 
-        # Extract relevant information from the response
         if response.status_code == 200:
             return jsonify({"message": "Solution uploaded successfully", "file_path": file_path}), 200
         else:
@@ -50,6 +59,7 @@ def get_solution():
     except Exception as e:
         print("File upload failed:", str(e))
         return jsonify({"error": "File upload failed", "details": str(e)}), 500
+
 
 
 @app.route('/getSolution', methods=['POST'])
@@ -79,4 +89,4 @@ def get_solution2():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8080)
