6c148772f7623dfa4d5036ec16ddc784e5c2688e
VianRey <vianreynecke@gmail.com>
2024-09-05 20:04:53 +0200

    Styling added on shipment menu for packer

--- src/components/PackerSidebar.vue
+++ src/components/PackerSidebar.vue
@@ -367,7 +367,7 @@ onMounted(() => {
           <Button
             @click="handleSelectShipment(shipment.id)"
             :class="[isDark ? 'text-white' : ' text-white', 'focus:outline-none focus:ring-0']"
-            class="text-lg justify-center px-4 py-2 w-full bg-green-800"
+            class="text-lg justify-center px-4 py-2 w-full bg-green-800 mt-2"
             >Select Delivery</Button
           >
         </div>
