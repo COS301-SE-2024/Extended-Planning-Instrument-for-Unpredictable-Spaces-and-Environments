646631d7579a5130a9b49ab135468eea0e454ad4
Mr-KSpolidoro <keganspoli@gmail.com>
2024-09-09 20:16:11 +0200

    fixing api query

--- supabase/functions/packing/fetchSolution.ts
+++ supabase/functions/packing/fetchSolution.ts
@@ -3,10 +3,7 @@ export async function fetchSolution(supabase: any, shipmentId: number) {
     // Generate the file name based on the shipment ID
     const fileName = `shipment_${shipmentId}.json`;
     const filePath = `solutions/${fileName}`; // Adjust this if the file is in a subfolder
-    
-    console.log('Attempting to fetch shipment solution from path:', filePath);
-
-    // Fetch the file from the 'solutions' bucket
+  
     const { data: fileData, error: fetchError } = await supabase.storage
       .from('solutions') // Ensure the bucket name is correct
       .download(filePath);
@@ -16,12 +13,8 @@ export async function fetchSolution(supabase: any, shipmentId: number) {
       return { error: 'Failed to retrieve shipment solution' };
     }
 
-    // Convert the file data to JSON (assuming it's a JSON file)
     const fileText = await fileData.text();
-    console.log('File content:', fileText);
-
     const jsonObject = JSON.parse(fileText);
-    console.log('Shipment solution retrieved successfully:', jsonObject);
 
     return { data: jsonObject };
   } catch (error) {
--- supabase/functions/packing/uploadSolution.ts
+++ supabase/functions/packing/uploadSolution.ts
@@ -1,28 +1,21 @@
 export async function uploadSolution(supabase: any, jsonObject: object, shipment_id: any) {
   try {
-    // Log the incoming JSON object for debugging
-    console.log('Incoming JSON Object:', jsonObject);
+    const jsonBlob = new Blob([JSON.stringify(jsonObject)], { type: 'application/json' })
 
-    // Convert the JSON object to a Blob format
-    const jsonBlob = new Blob([JSON.stringify(jsonObject)], { type: 'application/json' });
+    const fileName = `shipment_${shipment_id}.json`
 
-    // Generate a unique file name
-    const fileName = `shipment_${shipment_id}.json`;
-
-    // Upload the JSON file to the 'solutions' bucket in Supabase
     const { data: uploadData, error: uploadError } = await supabase.storage
       .from('solutions')
-      .upload(`solutions/${fileName}`, jsonBlob);
+      .upload(`solutions/${fileName}`, jsonBlob)
 
     if (uploadError) {
-      console.error('Error uploading solution:', uploadError);
-      return { error: 'Failed to upload solution' };
+      console.error('Error uploading solution:', uploadError)
+      return { error: `Failed to upload solution${uploadError}` }
+    } else {
+      return { data: uploadData }
     }
-
-    console.log('Solution uploaded successfully:', uploadData);
-    return { data: uploadData };
   } catch (error) {
-    console.error('Error processing solution:', error);
-    return { error: 'Error processing solution' };
+    console.error('Error processing solution:', error)
+    return { error: `Error processing solution${error}` }
   }
 }
