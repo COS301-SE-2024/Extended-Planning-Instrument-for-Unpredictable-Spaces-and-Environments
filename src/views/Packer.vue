2130855289c7f4f18b946d7603b11bcc194994d5
VianRey <vianreynecke@gmail.com>
2024-09-26 18:41:32 +0200

    Removed help button that was redundant

--- src/views/Packer.vue
+++ src/views/Packer.vue
@@ -1173,12 +1173,6 @@ async function generateNewSolution(shipmentID) {
           </div>
         </div>
       </div>
-      <p
-        @click="toggleDialogHelp"
-        class="flex items-center justify-center mt-2 text-orange-500 font-bold text-center hover:-translate-y-1 underline cursor-pointer transition duration-300"
-      >
-        Help
-      </p>
     </div>
   </div>
   <div>
